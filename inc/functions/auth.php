309b11b88b8524ebe48b65c172e78d9981864e8c
ribeir0dev <contatoreinaldomkt@gmail.com>
2025-12-20 00:10:07 -0300

    Delete inc directory

--- inc/functions/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-// inc/functions/auth.php
-
-if (!function_exists('gerarTokenPublico')) {
-    function gerarTokenPublico($length = 64) {
-        // Gera um token hex seguro, ex: 64 caracteres
-        return bin2hex(random_bytes($length / 2));
-    }
-}
